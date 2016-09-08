da1fcae489c3d19c285f48228320f3ff293bfab1
zhangqiang <1069507276@qq.com>
2016-09-08 10:29:07 +0800

    FirstPush

---
+++ TestXib/TestXib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestXib
+//
+//  Created by zhangqiang on 16/9/8.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestXib/TestXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestXib
+//
+//  Created by zhangqiang on 16/9/8.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
